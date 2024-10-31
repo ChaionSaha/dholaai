3abbf721c4717dd518c94765509c5ac27a7a5077
Chaion Saha <chaionsaha00@gmail.com>
2024-11-01 03:06:55 +0600

    added another couple of pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bangla: '"Hind Siliguri", serif',
+      },
+    },
   },
   plugins: [require("daisyui")],
 
